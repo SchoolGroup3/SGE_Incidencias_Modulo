386c168844c2c2d855c2f99b9fec19c9c0a33ba8
Mosi-HB-2006 <mosi.hickman2006@gmail.com>
2025-11-07 09:27:54 +0100

    Adding Many2Many field to employee herency

--- incidencias/models/Empleado.py
+++ incidencias/models/Empleado.py
@@ -4,3 +4,10 @@ class Empleado(models.Model):
     _inherit = "hr.employee"
 
     descripcion = fields.Text(string = 'Introduce la descripción')
+
+    id_incidencia = fields.Many2many(
+        comodel_name = 'incidencias.incidencia',
+        field_name = 'id_incidencia',
+        required=True, 
+        ondelete = 'cascade'
+    )
