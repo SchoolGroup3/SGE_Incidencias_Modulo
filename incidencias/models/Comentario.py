1fa2c3e690ef2e7824ba2bc1de89f32c55795671
Mosi-HB-2006 <mosi.hickman2006@gmail.com>
2026-01-29 10:48:14 +0100

    Adding Incidencia and Comentario functionalities

--- incidencias/models/Comentario.py
+++ incidencias/models/Comentario.py
@@ -1,12 +1,43 @@
 
-from odoo import fields, models
+import re
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class Comentario(models.Model):
     _name = 'incidencias.comentario'
     _description = 'Guarda los comentarios'
 
-    contenido = fields.Text(string = 'Introduce la descripción')
-    fecha = fields.Datetime(string = 'Fecha de hoy')
+    contenido = fields.Text(string = 'Descripción', required = True)
+    fecha = fields.Datetime(string = 'Fecha de hoy', required = True)
 
     id_incidencia = fields.Many2one(comodel_name = 'incidencias.incidencia', string = 'Incidencia', required = True, ondelete = 'cascade')
-    id_empleado = fields.Many2one(comodel_name= 'incidencias.employee', string = "Empleado", required = True, ondelete = "cascade")
+    id_empleado = fields.Many2one(comodel_name= 'hr.employee', string = "Empleado", required = True, ondelete = "cascade")
+
+    @api.onchange('fecha')
+    def _onchange_date(self):
+        if self.fecha:
+            if self.contenido:
+                # Mira si ya tiene una fecha en el titulo para actualizarlo usando un patron
+                self.contenido = re.sub(r'-\d{4}-\d{2}-\d{2}.*$', '', self.contenido)
+                self.contenido += f" - {str(self.fecha)}"
+            else:
+                self.contenido = f" - {str(self.fecha)}"
+
+    @api.constrains('contenido')
+    def _check_contenido_length(self):
+        if self.contenido and len(self.contenido) < 25:
+            raise ValidationError('El contenido debe ser mas de 25 caracteres')
+    
+    def copy(self, default=None):
+        if not default:
+            default={}
+
+        contenido = self.contenido or ''
+        contenido = re.sub(
+            r'^\(Copia de \d{4}-\d{2}-\d{2}(?: \d{2}:\d{2}:\d{2})?\)\s*',
+            '',
+            contenido
+        )
+        default['contenido'] = f"(Copia de {self.fecha or ''}) {contenido}"
+
+        return super().copy(default)
--- incidencias/models/Incidencia.py
+++ incidencias/models/Incidencia.py
@@ -1,15 +1,45 @@
-from odoo import fields, models
+import re
+from odoo import api, fields, models
+from odoo.exceptions import ValidationError
 
 class Incidencia(models.Model):
     _name = 'incidencias.incidencia'
     _description = 'Guarda las incidencias'
 
-    titulo = fields.Text(string = 'Introduce el titulo')
-    descripcion = fields.Text(string = 'Introduce la descripción')
-    fecha_creacion =fields.Datetime(string = 'Fecha de creación')
-    estado_actual = fields.Text(string = 'Introduce el estado actual')
+    titulo = fields.Text(string = 'Titulo', required = True)
+    descripcion = fields.Text(string = 'Descripción', required = True)
+    fecha_creacion =fields.Datetime(string = 'Fecha de creación', required = True)
+    estado_actual = fields.Selection([
+        ('cancelado', 'Cancelado'),
+        ('en_progreso', 'En Progreso'),
+        ('terminado', 'Terminado'),
+    ], string='Estado Actual', default='en_progreso')
 
-    id_departamento = fields.Many2one(comodel_name = 'hr.department', string = 'Incidencia', required = True, ondelete = 'cascade')
+    id_departamento = fields.Many2one(comodel_name = 'hr.department', string = 'Departamento', required = True, ondelete = 'cascade')
     id_empleado_origen = fields.Many2one(comodel_name= 'hr.employee', string = "Empleado", required = True, ondelete = "cascade")
 
     proyecto = fields.Many2one(comodel_name='project.task', string='Tarea de Proyecto', required=False, ondelete='cascade')
+    ids_comentarios = fields.One2many(comodel_name='incidencias.comentario', inverse_name='id_incidencia', string='Comentarios')
+
+    @api.onchange('fecha_creacion')
+    def _onchange_date(self):
+        if self.fecha_creacion:
+            self.titulo = re.sub(r'-\d{4}-\d{2}-\d{2}.*$', '', self.titulo)
+            self.titulo += f"-{str(self.fecha_creacion)}"
+
+    @api.constrains('titulo')
+    def _check_titulo_length(self):
+        if self.titulo and len(self.titulo) < 5:
+            raise ValidationError('El titulo debe ser mas de 5 caracteres')
+    
+    @api.model
+    def create(self, vals):
+        if 'titulo' in vals:
+            vals['titulo'] = vals['titulo'].replace(" ", "-")
+        
+        return super(Incidencia, self).create(vals)
+    def copy(self, default=None):
+        if not default:
+            default={}
+        default['titulo'] = f"Copia de {self.titulo}"
+        return super(Incidencia, self).copy(default)
